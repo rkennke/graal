8a08170f2a9ae47e1c95877243655c0b6e5fbc49
Robert Toyonaga <rtoyonag@redhat.com>
2025-01-08 16:50:44 -0500

    use skipcount and add new tests

--- substratevm/mx.substratevm/mx_substratevm.py
+++ substratevm/mx.substratevm/mx_substratevm.py
@@ -575,9 +575,8 @@ def native_unittests_task(extra_build_args=None):
             out.write(f"Simple file{i}" + '\n')
 
     additional_build_args = svm_experimental_options([
-        '-H:-JfrTrimInternalStackTraces',
-        '-H:AdditionalSecurityProviders=com.oracle.svm.test.SecurityServiceTest$NoOpProvider',
-        '-H:AdditionalSecurityServiceTypes=com.oracle.svm.test.SecurityServiceTest$JCACompliantNoOpService',
+        '-H:AdditionalSecurityProviders=com.oracle.svm.test.services.SecurityServiceTest$NoOpProvider',
+        '-H:AdditionalSecurityServiceTypes=com.oracle.svm.test.services.SecurityServiceTest$JCACompliantNoOpService',
         '-cp', cp_entry_name
     ])
     if extra_build_args is not None:
